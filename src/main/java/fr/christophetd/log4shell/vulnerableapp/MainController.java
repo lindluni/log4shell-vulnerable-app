989c9081037264e1b28cdc8e00220fe05c649416
Stefano <stefano.chierici@sysdig.com>
2021-12-15 18:38:28 +0100

    Added support to cookies, User-Agent

--- src/main/java/fr/christophetd/log4shell/vulnerableapp/MainController.java
+++ src/main/java/fr/christophetd/log4shell/vulnerableapp/MainController.java
@@ -1,12 +1,13 @@
 package fr.christophetd.log4shell.vulnerableapp;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import javax.servlet.http.*;
+
 
 @RestController
 public class MainController {
@@ -14,9 +15,27 @@ public class MainController {
     private static final Logger logger = LogManager.getLogger("HelloWorld");
 
     @GetMapping("/")
-    public String index(@RequestHeader("X-Api-Version") String apiVersion) {
-        logger.info("Received a request for API version " + apiVersion);
-        return "Hello, world!";
-    }
+//    public String index(@RequestHeader("X-Api-Version") String apiVersion) {
+//        logger.info("Received a request for API version " + apiVersion);
+//        return "Hello, world!";
+//    }
+
+
+    public String extractCookie(HttpServletRequest req) {
+        String userAgent = req.getHeader("User-Agent");
+        logger.info("Received a request with User-Agent: " + userAgent);
+
+        String apiVersion = req.getHeader("X-Api-Version");
+        logger.info("Received a request for API version: " + apiVersion);
+
+        for (Cookie c : req.getCookies()) {
+            logger.info("Received a request with Cookie: " + c.getValue());
+        }
+
+    return "Cookie Exploited";
+}
+
+    
+
 
 }
